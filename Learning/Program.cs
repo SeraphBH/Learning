321c21044caf63b9c326309e06a08ebed52498ba
Seraph <seraphbgi@gmail.com>
2016-06-22 01:15:28 +0200

    debut du menu

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -5,77 +5,106 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Learning
+class Program
 {
-    class Program
+    private string FormDuCadre1 = "vide";
+    private string FormDuCadre2 = "vide";
+
+    static void Main(string[] args)
     {
-        string FormDuCadre1 = "vide";
-        string FormDuCadre2 = "vide";
-        static void Main(string[] args)
-        {
+        Program myProgram = new Program();  // instance de programme permmetant de faire appel tout les tout les void publique
 
-            PersonalisationAleatoir();
-            CadreMenu();
-            MenuInteraction();
-            Console.ReadLine();
+        myProgram.Initialisation();
+
+         bool maBoucle = true;
+         while (maBoucle) // boucle permetant de lire les touches
+         {
+
+             if (Console.KeyAvailable)
+             {
+                 switch (Console.ReadKey().Key)
+                 {
+                     case ConsoleKey.F5:
+                         myProgram.Initialisation();
+                         break;
+                     case ConsoleKey.F6:
+                         Console.Clear();
+                         Console.ForegroundColor = ConsoleColor.White;
+                         Console.WriteLine("couscous");
+                         Thread.Sleep(2000);
+
+                         break;
+
+                 }
+
+             }
          }
-        static void CadreMenu()
-        {
-            Console.WriteLine(FormDuCadre1);
-            for (int i = 1; i <= 15; i++) 
-            {
-                Console.WriteLine(FormDuCadre2);
-            }
-            Console.WriteLine(FormDuCadre1);
-        }
-        static void MenuInteraction()
-        {
-            int x = 3; 
-            int y = 3;
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("Helloworld");
-            
-
-        }
-        static void PersonalisationAleatoir()
-
-
-        {
-            int maValeur = 0; 
-            for (int i = 1; i <= 5; i++)
-            {
-                maValeur = new Random().Next(1, 6);                               
-            }
-            switch (maValeur)
-                {
-                    case 1:                    
-                        FormDuCadre1 = "*******************************************************";
-                        FormDuCadre2 = "*                                                     *";
-                        break;
-                    case 2:
-                   
-                        FormDuCadre1 = "-------------------------------------------------------";
-                        FormDuCadre2 = "-                                                     -";
-                        break;
-                    case 3:
-                    
-                        FormDuCadre1 = "#######################################################";
-                        FormDuCadre2 = "#                                                     #";
-                        break;
-                    case 4:
-                    
-                        FormDuCadre1 = "=======================================================";
-                        FormDuCadre2 = "|                                                     |";
-                        break;
-                      case 5:
-                    
-                        FormDuCadre1 = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-                        FormDuCadre2 = "~                                                     ~";
-                        break;
-                }                                                                
-
-            }
-
-        }
+       
     }
 
+    public void Initialisation()
+    {
+        Program myProgram = new Program();
+        Console.Clear();
+        myProgram.PersonalisationAleatoir();
+        myProgram.CadreMenu();
+        myProgram.MenuInteraction();
+
+    }
+    public void CadreMenu()
+    {
+        Console.WriteLine(this.FormDuCadre1);
+        for (int i = 1; i <= 15; i++)
+        {
+            Console.WriteLine(this.FormDuCadre2);
+        }
+        Console.WriteLine(this.FormDuCadre1);
+    }
+
+    public void MenuInteraction()
+    {
+        int x = 3;
+        int y = 3;
+
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("Helloworld");
+    }
+
+    public void PersonalisationAleatoir()
+    {
+        int maValeur = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            maValeur = new Random().Next(1, 6);
+        }
+        switch (maValeur)
+        {
+            case 1:
+                this.FormDuCadre1 = "*******************************************************";
+                this.FormDuCadre2 = "*                                                     *";
+                break;
+            case 2:
+
+                this.FormDuCadre1 = "-------------------------------------------------------";
+                this.FormDuCadre2 = "-                                                     -";
+                break;
+            case 3:
+
+                this.FormDuCadre1 = "#######################################################";
+                this.FormDuCadre2 = "#                                                     #";
+                break;
+            case 4:
+
+                this.FormDuCadre1 = "=======================================================";
+                this.FormDuCadre2 = "|                                                     |";
+                break;
+            case 5:
+
+                this.FormDuCadre1 = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+                this.FormDuCadre2 = "~                                                     ~";
+                break;
+        }
+    }
+        
+}
